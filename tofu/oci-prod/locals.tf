e90498aaf1ab596d8c2fbada471372c9e21cf9e4
Christian Kirmse <cke@dasburo.com>
2025-07-17 14:10:41 +0200

    chore: delete old structure

--- tofu/oci-prod/locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  ADs = [
-    for i in data.oci_identity_availability_domains.ad.availability_domains : i.name
-  ]
-  core_image    = element(data.oci_core_images.ubuntu_image.images, 0)
-  core_image_id = local.core_image.id
-}
--- tofu/oci-prod/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "instance_ip_addresses" {
-  value = oci_core_instance.core_instance[*].public_ip
-}
