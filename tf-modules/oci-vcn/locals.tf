3f630a3f776e1925d5a2679828fa466e731905d6
Christian Kirmse <cke@dasburo.com>
2025-07-21 17:44:53 +0200

    chore: replace for operator in list with splat

--- tf-modules/oci-vcn/locals.tf
+++ tf-modules/oci-vcn/locals.tf
@@ -1,7 +1,5 @@
 locals {
-  ADs = [
-    for i in data.oci_identity_availability_domains.ad.availability_domains : i.name
-  ]
+  ADs               = data.oci_identity_availability_domains.ad.availability_domains[*].name
   core_image        = element(data.oci_core_images.ubuntu_image.images, 0)
   core_image_id     = local.core_image.id
   instance_backends = { for i in oci_core_instance.core_instance : i.display_name => i.private_ip }
